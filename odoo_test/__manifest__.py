235b782aae89338fd707a783e2623b4999f0df1a
Alcides Ojeda <eojedapilchik@gmail.com>
2021-09-14 01:12:20 +0000

    added basic module

---
+++ odoo_test/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Odoo Test",
+    "summary": "Odoo test Module to manage Training",
+    "description": """
+        Test Module to manage Training:
+        - Courses
+        - Sessions
+        - Others
+    """,
+    "author": "Alcides Ojeda",
+    "website": "https://zettave.com",
+    "category": "Training",
+    "version": "0.1",
+    "depends": ["base"],
+    "data":[],
+    "demo":[],
+    
+}
